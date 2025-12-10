81b532652bab88a3a0ce3672e71206ec07ddffb1
luizevm <101990232+luizevm@users.noreply.github.com>
2025-12-09 21:18:35 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.contrib import messages
+from .models import *
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class ObrasView(View):
+    def get(self, request, *args, **kwargs):
+        obras = Obra.objects.all()
+        return render(request, 'obras.html', {'obras': obras})
+
+
+class GenerosView(View):
+    def get(self, request, *args, **kwargs):
+        generos = Genero.objects.all()
+        return render(request, 'generos.html', {'generos': generos})
+
+
+class PlataformasView(View):
+    def get(self, request, *args, **kwargs):
+        plataformas = Plataforma.objects.all()
+        return render(request, 'plataformas.html', {'plataformas': plataformas})
--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,8 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('obras/', ObrasView.as_view(), name='obras'),
+    path('generos/', GenerosView.as_view(), name='generos'),
+    path('plataformas/', PlataformasView.as_view(), name='plataformas'),
 ]
