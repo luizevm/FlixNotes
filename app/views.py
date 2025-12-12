dba8e3bb5c82be825e39145da85d7db50fd01029
luizevm <101990232+luizevm@users.noreply.github.com>
2025-12-12 20:36:53 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import *
-
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -12,7 +13,10 @@ class IndexView(View):
 class ObrasView(View):
     def get(self, request, *args, **kwargs):
         obras = Obra.objects.all()
-        return render(request, 'obras.html', {'obras': obras})
+        return render(request, 'obras.html', {
+            'obras': obras,
+            'title': "Minhas Obras"
+        })
 
 
 class GenerosView(View):
@@ -25,3 +29,33 @@ class PlataformasView(View):
     def get(self, request, *args, **kwargs):
         plataformas = Plataforma.objects.all()
         return render(request, 'plataformas.html', {'plataformas': plataformas})
+
+
+class ObraCreateView(CreateView):
+    model = Obra
+    template_name = "obra_form.html"
+    fields = ["titulo", "tipo", "genero", "plataforma", "status", "nota", "comentario"]
+    success_url = reverse_lazy("obras")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Adicionar Obra"
+        return context
+
+
+class ObraUpdateView(UpdateView):
+    model = Obra
+    template_name = "obra_form.html"
+    fields = ["titulo", "tipo", "genero", "plataforma", "status", "nota", "comentario"]
+    success_url = reverse_lazy("obras")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Editar Obra"
+        return context
+
+
+class ObraDeleteView(DeleteView):
+    model = Obra
+    template_name = "obra_confirm_delete.html"
+    success_url = reverse_lazy("obras")
--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('obras/', ObrasView.as_view(), name='obras'),
+    path('obras/criar/', ObraCreateView.as_view(), name='obra_create'),
+    path('obras/<int:pk>/editar/', ObraUpdateView.as_view(), name='obra_update'),
+    path('obras/<int:pk>/deletar/', ObraDeleteView.as_view(), name='obra_delete'),
     path('generos/', GenerosView.as_view(), name='generos'),
     path('plataformas/', PlataformasView.as_view(), name='plataformas'),
 ]
